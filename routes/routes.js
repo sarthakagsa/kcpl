79f1b9a0f605cca29157bbe0914275732d8b51e8
sarthak <sarthakag.sa@gmail.com>
2020-07-07 23:06:07 +0530

    all errors fixed, login signup logout done

--- routes/routes.js
+++ routes/routes.js
@@ -6,6 +6,12 @@ var User = require("../models/user");
 
 var router = express.Router();
 
+router.use(function(req,res, next){
+    res.locals.currentUser = req.user;
+
+    next();
+});
+
 router.get("/",function(req,res){
     res.render("home");    
 });
@@ -14,36 +20,46 @@ router.get("/login",function(req,res){
     res.render("login");    
 });
 
+router.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/login");
+ });
+
+router.post("/login", passport.authenticate("login", {
+    successRedirect: "/",
+    failureRedirect: "/signup",
+    failureFlash: true
+ }));
+
 router.get("/signup",function(req,res){
     res.render("signup");    
 });
 
-router.post("/signup",function(req,res,next) {
+router.post("/signup", function (req, res, next) {
     var email = req.body.email;
     var password = req.body.password;
-
-    User.findOne({email:email},function(err,user) {
-        if (err) {
-            return next(err);
-        }
-        if (user) {
-            req.flash("error","Theres aldready an account with this email");
-            return res.redirect("/signup");
-        }
-
-        var newUser = new User({
-            email:email,
-            password:password
-        });
-        console.log(newUser);
-        newUser.save(next);
+ 
+    User.findOne({ email: email }, function (err, user) {
+       if (err) { return next(err); }
+       if (user) {
+          req.flash("error", "There's already an account with this email");
+          return res.redirect("/signup");
+       }
+ 
+       var newUser = new User({
+          password: password,
+          email: email
+       });
+ 
+       newUser.save(next);
+ 
     });
-},   passport.authenticate("login",{
-    successRedirect:"/",
-    failureRedirect : "/login",
-    failureFlash : true,
-})
-);
+ 
+ }, passport.authenticate("login", {
+    successRedirect: "/",
+    failureRedirect: "/signup",
+    failureFlash: true
+ }));
 
 
 router.use("/vechilepaper",require("./vechilepaper"));
--- setupPassport.js
+++ setupPassport.js
@@ -1,44 +1,41 @@
 var passport = require("passport");
-var Localstrategy = require("passport-local").Strategy;
+var LocalStrategy = require("passport-local").Strategy;
 
 var User = require("./models/user");
 
 module.exports = function () {
     //turns a user object into an id
-    passport.serializeUser(function (user,done) {
+    passport.serializeUser(function (user, done) {
         //serializing the user
-        done(null,user._id);    
+        done(null, user._id);
     });
-    //turns id into a user object
-    passport.deserializeUser(function (id,done) {
-        User.findById(id, function (err,user) {
-            done(err,user);
+    //turns the id into a user object
+    passport.deserializeUser(function (id, done) {
+        User.findById(id, function (err, user) {
+            done(err, user);
         });
     });
 
-    passport.use("login",new Localstrategy({
-        usernameField : "email",
-        passwordField : "password"
-    },function(email,password,done) {
-        User.findOne({email:email},function(err,user) {
-            if (err) {
-                return done(err);
-            }
+    passport.use("login", new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password'
+    }, function (email, password, done) {
+        User.findOne({ email: email }, function (err, user) {
+            if (err) { return done(err); }
             if (!user) {
-                return done(null,false,{message: "No user has that email"});
+                return done(null, false, { message: "No user has that Email!" });
             }
             user.checkPassword(password, function (err, isMatch) {
-                if (err) {
-                    return done(err);
-                }
+                if (err) { return done(err); }
                 if (isMatch) {
                     return done(null, user);
-                }
-                else{
-                    return done(null,false,{message:"invalid password"});
+                } else {
+                    return done(null, false, { message: "Invalid password" });
                 }
             });
         });
     }));
-    
+
+
+
 }
